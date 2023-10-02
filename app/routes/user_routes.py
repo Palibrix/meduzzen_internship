a540b79144cabb7595b4553dc4555a6b3e3858ae
palibrix <palibrix@gmail.com>
2023-10-02 22:03:01 +0300

    BE-5: Added Service class

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,58 +7,42 @@ from app.schemas import user_schema as schemas
 from app.models import user_model as model
 
 from app.db.database import get_session
-from app.services import user_service
+from app.services.user_service import UserService
 
 router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @router.get("/users/", response_model=schemas.UsersListResponse)
 async def get_users(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_session)):
-    result = await db.execute(select(model.User).offset(skip).limit(limit))
-    users = result.scalars().all()
-    return {"users": users}
+    service = UserService(db)
+    return {"users": await service.get_all_users(skip=skip, limit=limit)}
 
 
 @router.get("/users/{user_id}", response_model=schemas.UserDetailResponse)
 async def get_user(user_id: int, db: AsyncSession = Depends(get_session)):
-    # noinspection PyTypeChecker
-    user = await user_service.get_one_user(db=db, user_id=user_id)
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="User not found"
-                            )
-    return user
+    service = UserService(db)
+    return await service.get_one_user(user_id=user_id)
+
+
+@router.get("/me")
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    ...
 
 
 @router.post("/sign-up/", response_model=schemas.SignUpRequest)
 async def create_user(user: schemas.SignUpRequest, db: AsyncSession = Depends(get_session)):
-    db_user = await user_service.get_one_user(db=db, email=user.user_email)
-    if db_user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="User with this email already exist"
-                            )
-    new_user = await user_service.create_user(db, user)
-    return new_user
+    service = UserService(db)
+    return await service.create_user(user=user)
 
 
 @router.put("/users/{user_id}", response_model=schemas.UserUpdateRequest)
 async def update_user(user_id: int, user: schemas.UserUpdateRequest, db: AsyncSession = Depends(get_session)):
-    db_user = await user_service.get_one_user(db=db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="User not found"
-                            )
-    db_user = await user_service.update_user(db=db, db_user=db_user, user=user)
-    return db_user
+    service = UserService(db)
+    return await service.update_user(user_id=user_id, user=user)
 
 
 @router.delete("/users/{user_id}")
 async def delete_user(user_id: int, db: AsyncSession = Depends(get_session)):
-    db_user = await user_service.get_one_user(db=db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="User not found"
-                            )
-    await db.delete(db_user)
-    await db.commit()
-    return {"detail": "User deleted"}
+    service = UserService(db)
+    return await service.delete_user(user_id=user_id)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy import select, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,38 +8,68 @@ from app.models import user_model as model
 from app.schemas import user_schema as schemas
 
 
-async def get_one_user(db: AsyncSession, user_id: int = None, email: str = None):
-	if user_id is None and email is None:
-		raise ValueError("Either 'id' or 'email' must be provided")
-	email = email.lower() if email else None
-	stmt = select(model.User).where(or_(model.User.user_id == user_id, model.User.user_email == email))
-	result = await db.execute(stmt)
-	user = result.scalars().first()
+class UserService:
 
-	return user
+	def __init__(self, db: AsyncSession):
+		self.db = db
 
+	async def get_all_users(self, skip: int, limit: int):
+		result = await self.db.execute(select(model.User).offset(skip).limit(limit))
+		return result.scalars().all()
 
-async def create_user(db: AsyncSession, user: schemas.SignUpRequest):
-	pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+	async def get_one_user(self, user_id: int = None, email: str = None):
+		if not user_id and not email:
+			raise ValueError("Either 'id' or 'email' must be provided")
+		email = email.lower() if email else None
+		stmt = select(model.User).where(or_(model.User.user_id == user_id, model.User.user_email == email))
+		result = await self.db.execute(stmt)
+		user = result.scalars().first()
 
-	normalized_email = user.user_email.lower()
-	hashed_password = pwd_context.hash(user.hashed_password)
+		if user is None:
+			raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+								detail="User not found"
+								)
 
-	user_dict = user.model_dump()
+		return user
 
-	user_dict["user_email"] = normalized_email
-	user_dict["hashed_password"] = hashed_password
+	async def create_user(self, user: schemas.SignUpRequest):
+		pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-	new_user = model.User(**user_dict)
-	db.add(new_user)
-	await db.commit()
-	await db.refresh(new_user)
-	return new_user
+		try:
+			await self.get_one_user(email=user.user_email)
+		except HTTPException as e:
+			if e.status_code == status.HTTP_404_NOT_FOUND:
+				...
+		else:
+			raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+								detail="User with this email already exist"
+								)
 
+		normalized_email = user.user_email.lower()
+		hashed_password = pwd_context.hash(user.hashed_password)
 
-async def update_user(db: AsyncSession, db_user, user: schemas.UserUpdateRequest):
-	for key, value in user.model_dump().items():
-		if value is not None:
-			setattr(db_user, key, value)
-	await db.commit()
-	return db_user
+		user_dict = user.model_dump()
+
+		user_dict["user_email"] = normalized_email
+		user_dict["hashed_password"] = hashed_password
+
+		new_user = model.User(**user_dict)
+		self.db.add(new_user)
+		await self.db.commit()
+		await self.db.refresh(new_user)
+		return new_user
+
+	async def update_user(self, user_id: int, user: schemas.UserUpdateRequest):
+		db_user = await self.get_one_user(user_id=user_id)
+		for key, value in user.model_dump().items():
+			if value is not None:
+				setattr(db_user, key, value)
+		await self.db.commit()
+		return db_user
+
+	async def delete_user(self, user_id: int):
+		db_user = await self.get_one_user(user_id=user_id)
+		await self.db.delete(db_user)
+		await self.db.commit()
+		return {"detail": "User deleted"}
+
